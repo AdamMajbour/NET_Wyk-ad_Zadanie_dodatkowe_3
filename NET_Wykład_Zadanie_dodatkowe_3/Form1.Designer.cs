f4c16b293f9930a7ba1f038827eabe803830025c
Adam Majbour-Majek <adam.majbourmajek@student.wat.edu.pl>
2021-01-03 23:00:43 +0100

    Dodaj pliki projektów.

---
+++ NET_Wykład_Zadanie_dodatkowe_3/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace NET_Wykład_Zadanie_dodatkowe_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addressURLtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // addressURLtextBox
+            // 
+            this.addressURLtextBox.Location = new System.Drawing.Point(16, 29);
+            this.addressURLtextBox.Multiline = true;
+            this.addressURLtextBox.Name = "addressURLtextBox";
+            this.addressURLtextBox.Size = new System.Drawing.Size(415, 28);
+            this.addressURLtextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press the address here:";
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(16, 64);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 2;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(112, 69);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Synchronously";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(214, 69);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(100, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Asynchronously";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(16, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Progress:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(19, 111);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(412, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(19, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 158);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(409, 99);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 269);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addressURLtextBox);
+            this.Name = "Form1";
+            this.Text = "Zadanie 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox addressURLtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ NET_Wykład_Zadanie_dodatkowe_3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NET_Wykład_Zadanie_dodatkowe_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeToTextbox(string address)
+        {
+            var numberOfCharacters = File.ReadAllText("test.txt").Count(c => c.Equals(" ") || (!Char.IsControl(c) && !Char.IsWhiteSpace(c)));
+            var fileSize = new System.IO.FileInfo("test.txt").Length;
+
+            textBox2.Text = "Address URL: " + address + Environment.NewLine + "Number of characters: " + numberOfCharacters + Environment.NewLine + "Size of file: " + fileSize;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string address;
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                MessageBox.Show("Check only one chceckBox!");
+            }
+            else if (checkBox1.Checked && !checkBox2.Checked)
+            {
+                address = addressURLtextBox.Text;
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.DownloadProgressChanged += (s, e2) => { progressBar1.Value = e2.ProgressPercentage; };
+                    webClient.DownloadFile(address, "test.txt");
+                }
+                writeToTextbox(address);
+            }
+            else if (!checkBox1.Checked && checkBox2.Checked)
+            {
+                address = addressURLtextBox.Text;
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(delegate (object sender2, DownloadProgressChangedEventArgs e2)
+                    {
+                        progressBar1.Value = e2.ProgressPercentage;
+                    });
+                    webClient.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(delegate (object sender3, System.ComponentModel.AsyncCompletedEventArgs e3)
+                        {
+                            if(e3.Error == null && !e3.Cancelled)
+                            {
+                                writeToTextbox(address);
+                            }
+                        });
+                    webClient.DownloadFileAsync(new Uri(address), "test.txt");
+                }
+            }
+            else MessageBox.Show("Check synchronously or asynchronously");
+        }
+    }
+}
